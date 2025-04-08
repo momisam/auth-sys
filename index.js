0cb1367750b970116345f7ea6e323c01ae0bb7b9
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-04-08 14:22:18 +0100

    all dep

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const helmet = require('helmet')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 const app = express()
-app.use(helmet)
-app.use(cors)
+app.use(helmet())
+app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
+
 app.get('/', (req,res) => {
     res.json({message: 'Hello from the server'})
 })
