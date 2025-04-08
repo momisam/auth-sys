d44bce6c0b34e1ccf3f0e0f172f9ea2c297c57be
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-04-08 21:25:34 +0100

    database connected

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.urlencoded({extended: true}))
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Database Connecting ..........');
+    
+})
 
 app.get('/', (req,res) => {
     res.json({message: 'Hello from the server'})
