f4fb27a52cb1e1a5656dee1245494fb11846a62f
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-04-09 10:41:24 +0100

    model schema commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,24 @@
+const { types, required } = require('joi')
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema(
     {
         email:{
             types: String,
-            required: true
+            required: [true, 'Email is required'],
+            trim: true,
+            unique: [true, 'Email must be unique'],
+            minLength: [5, 'Email must have 5 characters'],
+            lowercase: true,
+        },
+        password:{
+            types: String,
+            required: [true, 'Password must be provided'],
+            trim: true,
+            select: false,
+        },
+        verified:{
+            
         }
     }
 )
