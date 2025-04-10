a804846e7b2eab8117e9bef1ff4091a9165f1e5f
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-04-10 13:05:55 +0100

    auth validator

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,3 +1,22 @@
+const { signupSchema } = require("../middleware/validator");
+
 exports.signup = async (req, res) => {
     const {email,password} = req.body;
+    try {
+        const {error, value} = signupSchema.validate({email,password});
+
+        if(error){
+            return res.status(401.json({success:false, message: error.details[0].message}))
+        }
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return res.status(4011).json({success:false, message: 'User already exist!'})
+        }
+
+        
+    } catch (error) {
+        console.log(error);
+    
+    }
 } 
